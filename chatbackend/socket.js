59ba85e144f8504539affe4808aa446e685c55ef
sai <vv92343@gmail.com>
2025-07-12 23:17:24 +0530

    updated cors in socket

--- chatbackend/socket.js
+++ chatbackend/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+   origin: ['https://regal-gingersnap-963629.netlify.app'],
     methods: ["GET", "POST"],
     credentials: true
   }
